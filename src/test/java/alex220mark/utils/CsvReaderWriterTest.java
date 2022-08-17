3438c8340d3dfa0e9a9326358f4e0360b51445b6
unknown <alex220mark@gmail.com>
2022-08-17 13:12:43 +0200

    test removal of leading/trailing and middle whitespaces

--- src/test/java/alex220mark/utils/CsvReaderWriterTest.java
+++ src/test/java/alex220mark/utils/CsvReaderWriterTest.java
@@ -1,11 +1,14 @@
 package alex220mark.utils;
 
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CsvReaderWriterTest {
-	
-	// TODO: test whitespace removal
+
 	// TODO: test capitalisation of Strings
 
 	private CsvReaderWriter underTest;
@@ -14,12 +17,16 @@ public class CsvReaderWriterTest {
 	void setUp() {
 		underTest = new CsvReaderWriter();
 	}
-	
+
 	@Test
 	void itShouldRemoveAllWhiteSpace() {
-		
-		
-		
+		List<String> spaces = List.of("red socks", "red socks", "red socks", "red socks", "red socks");
+		List<String> spacesRemoved = List.of(underTest.removeWhiteSpaceFromString(" red socks"),
+				underTest.removeWhiteSpaceFromString("red socks  "),
+				underTest.removeWhiteSpaceFromString("  red socks "),
+				underTest.removeWhiteSpaceFromString(" red    socks "),
+				underTest.removeWhiteSpaceFromString("red   socks"));
+		assertLinesMatch(spaces, spacesRemoved);
 	}
 
 }
