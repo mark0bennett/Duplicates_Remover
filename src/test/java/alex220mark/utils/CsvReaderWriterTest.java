c167665626eff50d63016687fc2294733bc282e6
unknown <alex220mark@gmail.com>
2022-08-17 12:27:47 +0200

    add junit test as a dependency in pom.xml

---
+++ src/test/java/alex220mark/utils/CsvReaderWriterTest.java
@@ -0,0 +1,14 @@
+package alex220mark.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class CsvReaderWriterTest {
+
+	private CsvReaderWriter underTest;
+
+	@BeforeEach
+	void setUp() {
+		underTest = new CsvReaderWriter();
+	}
+
+}
