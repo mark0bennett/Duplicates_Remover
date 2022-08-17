85092b12c5905d8734f8e3582b2d4520efd5a198
unknown <alex220mark@gmail.com>
2022-08-17 13:17:41 +0200

    test capitalisation of Strings in CsvReaderWriter

--- src/test/java/alex220mark/utils/CsvReaderWriterTest.java
+++ src/test/java/alex220mark/utils/CsvReaderWriterTest.java
@@ -4,19 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CsvReaderWriterTest {
 
-	// TODO: test capitalisation of Strings
-
 	private CsvReaderWriter underTest;
-
-	@BeforeEach
-	void setUp() {
-		underTest = new CsvReaderWriter();
-	}
 
 	@Test
 	void itShouldRemoveAllWhiteSpace() {
@@ -26,6 +18,18 @@ public class CsvReaderWriterTest {
 				underTest.removeWhiteSpaceFromString("  red socks "),
 				underTest.removeWhiteSpaceFromString(" red    socks "),
 				underTest.removeWhiteSpaceFromString("red   socks"));
+		assertLinesMatch(spaces, spacesRemoved);
+	}
+
+	@Test
+	void itShouldCapitalise() {
+		List<String> spaces = List.of("Red Socks", "Red Socks", "Red Socks", "Red Socks", "Red Socks", "Red Socks");
+		List<String> spacesRemoved = List.of(underTest.capitalizeString("red socks"),
+				underTest.capitalizeString("Red socks"),
+				underTest.capitalizeString("red Socks"),
+				underTest.capitalizeString("Red Socks"),
+				underTest.capitalizeString("reD soCKs"),
+				underTest.capitalizeString("rEd SOckS"));
 		assertLinesMatch(spaces, spacesRemoved);
 	}
 
